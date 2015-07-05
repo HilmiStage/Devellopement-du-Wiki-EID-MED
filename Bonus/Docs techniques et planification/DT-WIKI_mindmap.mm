5db1626ba48ec2ebed8c454b02f71f6337ffcf99
HilmiStage <hilmi.bouallegue@gmail.com>
2015-07-05 12:35:59 +0200

    Ajouter le dossier Bonus

---
+++ Bonus/Docs techniques et planification/DT-WIKI_mindmap.mm
@@ -0,0 +1,600 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1428504534015" ID="ID_275266864" MODIFIED="1428506018475" TEXT="WIKI-DT">
+<node CREATED="1428504576280" FOLDED="true" HGAP="-231" ID="ID_104403320" MODIFIED="1428569322707" POSITION="right" TEXT="Outils/Maintenance/Administration" VSHIFT="44">
+<node CREATED="1428504694587" ID="ID_503128333" MODIFIED="1428509644344">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Comparatif / s&#233;lection des extensions les plus utiles
+    </p>
+    <p>
+      Objectif des extensions:
+    </p>
+    <p>
+      faciliter l'&#233;dition
+    </p>
+    <p>
+      faciliter la recherche
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1428504720108" FOLDED="true" ID="ID_1846573845" MODIFIED="1428569308012" TEXT="Environnement applicatif">
+<node CREATED="1428507869279" ID="ID_25279242" MODIFIED="1428508134559">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Environnement de d&#xe9;veloppement
+    </p>
+    <p>
+      dt-wiki-dev
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1428507898045" ID="ID_1137933548" MODIFIED="1428508149691">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Environnement de production
+    </p>
+    <p>
+      dt-wiki-prod
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1428507914160" ID="ID_164542860" MODIFIED="1428508076355">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Mediawiki 1.24.2
+      </li>
+      <li>
+        postgresql-9.2.4
+      </li>
+      <li>
+        xampp-5.6.3
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1428504627369" ID="ID_1716834038" MODIFIED="1428504646438" POSITION="left" TEXT="Droits utilisateurs / gestion &#xe9;ditoriale"/>
+<node CREATED="1428504651617" ID="ID_1039028277" MODIFIED="1428504691837" POSITION="right" TEXT="Gestion du contenu">
+<node CREATED="1428504775974" ID="ID_1646530519" MODIFIED="1428569172802">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Squelette
+    </p>
+    <p style="text-align: center">
+      Arborescence du wiki
+    </p>
+    <p style="text-align: center">
+      Description s&#233;mantique
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1428504826400" ID="ID_208566208" MODIFIED="1428567685756" TEXT="Accueil G&#xe9;n&#xe9;ral">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1428567984013" ID="ID_2899363" MODIFIED="1428569305984" TEXT="Portails" VSHIFT="-46">
+<node CREATED="1428567327080" ID="ID_1174465376" MODIFIED="1428567348811" TEXT="Portail Sant&#xe9; publique"/>
+<node CREATED="1428567356470" ID="ID_57496970" MODIFIED="1428567372476" TEXT="Portail D&#xe9;moustication (op&#xe9;rationnelle)"/>
+<node CREATED="1428567377094" ID="ID_1235992731" MODIFIED="1428567395704" TEXT="Portail R&amp;D"/>
+<node CREATED="1428567401866" ID="ID_1225873414" MODIFIED="1428567425890" TEXT="Portail Littoral"/>
+<node CREATED="1428567465343" ID="ID_1460137481" MODIFIED="1428567486419" TEXT="Portail Laboratoire"/>
+<node CREATED="1428567494390" ID="ID_1639779874" MODIFIED="1428569372580" TEXT="Portail R&#xe9;seau scientifique"/>
+</node>
+<node CREATED="1428567818387" HGAP="155" ID="ID_939081991" MODIFIED="1428571561990" TEXT="Cat&#xe9;gories" VSHIFT="-134">
+<node CREATED="1428567828168" ID="ID_976085913" MODIFIED="1428569348573">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Entomologie
+    </p>
+    <ul>
+      <li>
+        biologie
+      </li>
+      <li>
+        &#233;cologie
+      </li>
+      <li>
+        syst&#232;matique
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1428567835469" ID="ID_1836673279" MODIFIED="1428571636194">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Surveillance entomologique
+    </p>
+    <ul>
+      <li>
+        r&#233;glements
+      </li>
+      <li>
+        m&#233;thodes de surveillance (protocoles)
+      </li>
+      <li>
+        SI - tra&#231;abilit&#233; (modalit&#233;s de recueil / exploitation / stockage des donn&#233;es [aspects spatio-temporels])
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1428572191152" ID="ID_785924657" MODIFIED="1428572235221">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Entomologie m&#233;dicale
+    </p>
+    <ul>
+      <li>
+        &#233;pid&#233;miologie
+      </li>
+      <li>
+        virologie
+      </li>
+      <li>
+        bact&#233;riologie
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1428569393983" ID="ID_839115191" MODIFIED="1428570862106">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Biocides
+    </p>
+    <ul>
+      <li>
+        exp&#233;rimentation (comportement efficacit&#233;)
+      </li>
+      <li>
+        formulation
+      </li>
+      <li>
+        synergies
+      </li>
+      <li>
+        r&#233;glementation
+      </li>
+      <li>
+        stockage
+      </li>
+      <li>
+        Hygi&#232;ne et S&#233;curit&#233;
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1428571283743" ID="ID_1136460044" MODIFIED="1428572299336">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      sociologie perception nuisance
+    </p>
+    <p>
+      r&#233;ception du message de pr&#233;vention...
+    </p>
+    <p>
+      formation aux bonnes pratiques
+    </p>
+    <p>
+      outils p&#233;dagogiques
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="help"/>
+</node>
+<node CREATED="1428571564002" ID="ID_448681994" MODIFIED="1428571882369">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Effets non intentionnels
+    </p>
+    <ul>
+      <li>
+        toxicologie
+      </li>
+      <li>
+        &#233;co-toxicologie
+      </li>
+      <li>
+        d&#233;rangement
+      </li>
+      <li>
+        Natura 2000
+      </li>
+      <li>
+        r&#233;glementation
+      </li>
+      <li>
+        exp&#233;rimentation
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1428571934129" ID="ID_169852291" MODIFIED="1428571976216">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Gestion des risques
+    </p>
+    <ul>
+      <li>
+        plan de gestion de crise albo
+      </li>
+      <li>
+        perception nuisance
+      </li>
+      <li>
+        r&#233;glementation
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1428571987245" ID="ID_1973859500" MODIFIED="1428572164227">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Littoral
+    </p>
+    <p>
+      travaux am&#233;nagement
+    </p>
+    <p>
+      prostection des dunes
+    </p>
+    <p>
+      rv&#233;g&#233;atilsation*
+    </p>
+    <p>
+      suivi trait de c&#244;te et recul strat&#233;gique
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1428570712003" ID="ID_912261142" MODIFIED="1428571603857">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      M&#233;thodes de lutte
+    </p>
+    <ul>
+      <li>
+        biocide
+      </li>
+      <li>
+        biologique
+      </li>
+      <li>
+        g&#233;n&#233;tique
+      </li>
+      <li>
+        int&#233;gr&#233;e
+      </li>
+      <li>
+        physique
+      </li>
+      <li>
+        r&#233;pulsif
+      </li>
+      <li>
+        participation communautaire
+      </li>
+      <li>
+        techniques de pulv&#233;risation
+      </li>
+      <li>
+        techniques de mesure de l'efficacit&#233;
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1428569685603" ID="ID_307920648" MODIFIED="1428569819199" TEXT="Articles">
+<arrowlink DESTINATION="ID_307920648" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_355515299" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_307920648" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_355515299" SOURCE="ID_307920648" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<node CREATED="1428569699252" ID="ID_192762027" MODIFIED="1428570464711">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Mod&#232;le Guide des Bonnes Pratiques
+    </p>
+    <ul>
+      <li>
+        GBP LIFE
+      </li>
+      <li>
+        GBP gestion de l'eau - milieux naturels
+      </li>
+      <li>
+        GBP gestion de l'eau - milieux urbains
+      </li>
+      <li>
+        GBP Professionnels du b&#226;timent
+      </li>
+      <li>
+        Vademecum labo confin&#233;
+      </li>
+      <li>
+        Vademecum labo conventionnel
+      </li>
+      <li>
+        Vademecum LAV
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1428569820993" ID="ID_1858544781" MODIFIED="1428570260802">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Mod&#232;le encyclop&#233;dique
+    </p>
+    <ul>
+      <li>
+        description format&#233;e d'une notion utilis&#233;e
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1428569897619" ID="ID_1104461346" MODIFIED="1428570203675">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Mod&#232;le note de synth&#232;se
+    </p>
+    <ul>
+      <li>
+        point sur un projet (d&#233;roulement)
+      </li>
+      <li>
+        &#233;v&#232;nementiel
+      </li>
+      <li>
+        &#233;dito (avis / vue de quelqu'un sur un sujet)
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="yes"/>
+</node>
+<node CREATED="1428569996975" ID="ID_1560715903" MODIFIED="1428570125847">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Mod&#232;le fiche m&#233;thodologique
+    </p>
+    <ul>
+      <li>
+        protocole
+      </li>
+      <li>
+        fiche d'utilisation
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1428567624838" FOLDED="true" ID="ID_619739842" MODIFIED="1428567728110" TEXT="essai 22 01 2015">
+<icon BUILTIN="clanbomber"/>
+<node CREATED="1428504848378" FOLDED="true" ID="ID_510889079" MODIFIED="1428567487713" TEXT="Portail Entomologie">
+<node CREATED="1428505233407" ID="ID_809310053" MODIFIED="1428505244300" TEXT="Cat&#xe9;gories">
+<node CREATED="1428505258728" ID="ID_1931314068" MODIFIED="1428505265468" TEXT="Articles"/>
+</node>
+</node>
+<node CREATED="1428504986262" ID="ID_1518885340" MODIFIED="1428505005654" TEXT="Portail Lutte"/>
+<node CREATED="1428504988238" ID="ID_1259298579" MODIFIED="1428505053839" TEXT="Portail Recherche &amp; d&#xe9;veloppement"/>
+<node CREATED="1428504990326" ID="ID_409064315" MODIFIED="1428505067404" TEXT="Portail Environnement"/>
+<node CREATED="1428504992838" ID="ID_1325089150" MODIFIED="1428505077086" TEXT="Portail Littoral"/>
+<node CREATED="1428504996118" ID="ID_1736204385" MODIFIED="1428505087871" TEXT="Portail Gestion des risques"/>
+<node CREATED="1428505090586" ID="ID_601496293" MODIFIED="1428505112208" TEXT="Portail Bonnes Pratiques"/>
+</node>
+</node>
+<node CREATED="1428504775974" HGAP="30" ID="ID_489061887" MODIFIED="1428568443293" VSHIFT="103">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Squelette
+    </p>
+    <p style="text-align: center">
+      Description composants du wiki
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1428504826400" ID="ID_730512911" MODIFIED="1428504843910" TEXT="Accueil G&#xe9;n&#xe9;ral">
+<node CREATED="1428506131413" ID="ID_453228513" MODIFIED="1428507772559">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Point d'entr&#xe9;e du wiki
+    </p>
+    <p>
+      Composition :
+    </p>
+    <ul>
+      <li>
+        Pr&#xe9;sente le wiki (quoi et pourquoi)
+      </li>
+      <li>
+        Distribue l'acc&#xe8;s aux portails (7)
+      </li>
+      <li>
+        Propose une s&#xe9;lection d'articles (article du mois / dernier article...)
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1428504848378" ID="ID_1603861713" MODIFIED="1428505683506" TEXT="Portail ">
+<node CREATED="1428506693131" ID="ID_476620686" MODIFIED="1428507742248">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Entr&#xe9;e th&#xe9;matique
+    </p>
+    <p>
+      Les diff&#xe9;rents portails sont fix&#xe9;s a priori
+    </p>
+    <p>
+      Composition :
+    </p>
+    <ul>
+      <li>
+        Pr&#xe9;sentation du portail (d&#xe9;finition)
+      </li>
+      <li>
+        Liste des cat&#xe9;gories
+      </li>
+      <li>
+        Dernier article
+      </li>
+      <li>
+        rappel des liens vers les autres portails th&#xe9;matiques
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1428505233407" ID="ID_1849996966" MODIFIED="1428505244300" TEXT="Cat&#xe9;gories">
+<node CREATED="1428508317934" ID="ID_1617622142" MODIFIED="1428508457055">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Cat&#xe9;gories transversales :
+    </p>
+    <p>
+      peuvent se retrouver dans plusieurs portails
+    </p>
+    <p>
+      &#xe0; d&#xe9;finir
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1428509221250" ID="ID_705662418" MODIFIED="1428509277441">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Cat&#xe9;gories sp&#xe9;cifiques :
+    </p>
+    <p>
+      plut&#xf4;t un r&#xf4;le de mot-cl&#xe9;
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1428505258728" ID="ID_1157762302" MODIFIED="1428505265468" TEXT="Articles">
+<node CREATED="1428562561480" ID="ID_423980287" MODIFIED="1428569286093" TEXT="Typologie d&apos;articles">
+<icon BUILTIN="help"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
